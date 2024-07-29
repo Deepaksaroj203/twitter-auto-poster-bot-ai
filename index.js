8ea997c306ed41a7a523d9c88a96fa129be18d6d
Deepaksaroj203 <35185812+Deepaksaroj203@users.noreply.github.com>
2024-07-28 23:18:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate a web development content, tips and tricks or something new or some rant or some advice as a tweet, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    "generate a recent indian polity news, advices and question ask on those news or social issue or something new, it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis and trending hastags related to the news";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
